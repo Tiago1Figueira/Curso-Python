07ccc052f32166b045da8e314304c294baed590e
moderno <tiago_figueira@hotmail.com>
2022-09-15 18:01:11 -0300

    update ex40

--- secao5/exercicios5/Ex40.py
+++ secao5/exercicios5/Ex40.py
@@ -1,3 +1,4 @@
+"""
 #1
 comissao1 = 0.05
 imposto1 = 0.00
@@ -18,4 +19,29 @@ while True:
         comissao = custo_fabrica * comissao3
         imposto = custo_fabrica * imposto3
     print(f'Custo de fábrica {custo_fabrica:.2f}\ncomissão do distribuidor {comissao:.2f}\nimpostos {imposto:.2f}')
+"""
+comissao1 = 0.05
+comissao2 = 0.10
+comissao3 = 0.15
+imposto1 = 0
+imposto2 = 0.15
+imposto3 = 0.20
+while True:
+    print('%' * 50, 'CÁLCULO DO CUSTO DE UM CARRO AO CONSUMIDOR:', '%' * 50)
+    custo_de_fabrica = float(input('Insira o valor do custo de fábrica do carro: R$'))
+    if custo_de_fabrica <= 1200:
+        valor_carro1 = custo_de_fabrica + (custo_de_fabrica * imposto1) + (custo_de_fabrica * comissao1)
+        print(f'O valor do carro ao consumidor será R$ {valor_carro1:.2f}.')
+    elif custo_de_fabrica > 1200 and custo_de_fabrica <= 2500:
+        valor_carro2 = custo_de_fabrica + (custo_de_fabrica * imposto2) + (custo_de_fabrica * comissao2)
+        print(f'O valor do carro ao consumidor será R$ {valor_carro2:.2f}')
+    else:
+        valor_carro3 = custo_de_fabrica + (custo_de_fabrica * imposto3) + (custo_de_fabrica * comissao3)
+        print(f'O valor do carro ao consumidor será R$ {valor_carro3:.2f}')
+
+
+
+
+
+
 
